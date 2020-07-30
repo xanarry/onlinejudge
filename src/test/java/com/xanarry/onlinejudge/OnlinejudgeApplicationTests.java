6b454dde1c3be00c4fcbfa9c49c6904aa5adf90d
xanarry <xiong-yang@qq.com>
2020-07-30 23:50:36 +0800

    Initial commit

---
+++ src/test/java/com/xanarry/onlinejudge/OnlinejudgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xanarry.onlinejudge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinejudgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
